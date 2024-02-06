0265a7416229f060b836cd959623d553a9912158
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2024-02-06 22:02:14 +0900

    Add: light_gray, shape 2개, bottom_sheet_dialog.xml, edit 추가, myvideo_item.xml, MyVideoAdapter 추가, MyVideoFragment binding 추가 Change: fragment_my_video.xml Ui 고침

--- app/src/main/java/com/android/traveltube/mypage/MyVideoFragment.kt
+++ app/src/main/java/com/android/traveltube/mypage/MyVideoFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.traveltube.R
-
+import com.android.traveltube.databinding.FragmentMyVideoBinding
 
 class MyVideoFragment : Fragment() {
+
+    private var binding: FragmentMyVideoBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_video, container, false)
+        return binding?.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
