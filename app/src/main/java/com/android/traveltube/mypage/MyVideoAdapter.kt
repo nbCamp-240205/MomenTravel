9c590d31c6d2581f740a1709a0e4d6d92d2f5604
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2024-02-07 15:25:35 +0900

    Feat: ic_airport.png 사진 추가, gradient_edit.xml 그라데이션 추가 Change: fragment_my_video.xml Ui 수정

--- app/src/main/java/com/android/traveltube/mypage/MyVideoAdapter.kt
+++ app/src/main/java/com/android/traveltube/mypage/MyVideoAdapter.kt
@@ -1,20 +1,50 @@
 package com.android.traveltube.mypage
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.android.traveltube.R
 
-class MyVideoAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MyVideoAdapter(
+    private val itemList: List<MyVideoItem>,
+    private val onItemClick: (position: Int) -> Unit,
+    private val onItemLongClick: (position: Int) -> Unit
+) : RecyclerView.Adapter<MyVideoAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.myvideo_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = itemList[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return itemList.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    fun getItem(position: Int) {
+
+    }
+
+    fun removeItem(position: Int) {
+
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+
+            itemView.setOnClickListener {
+                onItemClick(adapterPosition)
+            }
+
+            itemView.setOnLongClickListener {
+                onItemLongClick(adapterPosition)
+                true
+            }
+        }
     }
 }
--- app/src/main/java/com/android/traveltube/mypage/MyVideoFragment.kt
+++ app/src/main/java/com/android/traveltube/mypage/MyVideoFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.traveltube.databinding.FragmentMyVideoBinding
 
 class MyVideoFragment : Fragment() {
 
     private var binding: FragmentMyVideoBinding? = null
+    private lateinit var adapter: MyVideoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentMyVideoBinding.inflate(inflater, container, false)
+
+        val recyclerView = binding?.rvMyVideoMyHistory
+        val layoutManager = LinearLayoutManager(requireContext())
+        recyclerView?.layoutManager = layoutManager
+
         return binding?.root
     }
 
